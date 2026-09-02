c03ed2ceec26576f7f78a007d5791cd3f00166c7
ksy00051087 <ksy00051087@gmail.com>
2026-09-02 09:17:36 +0900

    [Passed] 비교 연산 (49ms, 16MB)

---
+++ trail1/비교 연산/comparison-operator.py
@@ -0,0 +1,37 @@
+a, b = map(int, input().split())
+
+# a >= b
+if a >= b:
+    print(1)
+else:
+    print(0)
+
+# a > b
+if a > b:
+    print(1)
+else:
+    print(0)
+
+# a <= b
+if a <= b:
+    print(1)
+else:
+    print(0)
+
+# a < b
+if a < b:
+    print(1)
+else:
+    print(0)
+
+# a == b
+if a == b:
+    print(1)
+else:
+    print(0)
+
+# a != b
+if a != b:
+    print(1)
+else:
+    print(0)
