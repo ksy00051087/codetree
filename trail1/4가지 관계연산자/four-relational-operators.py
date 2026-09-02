2c6f28c0c40571ae3bbd83319c14679558ce6f32
ksy00051087 <ksy00051087@gmail.com>
2026-09-02 09:34:05 +0900

    [Passed] 4가지 관계연산자 (51ms, 16MB)

---
+++ trail1/4가지 관계연산자/four-relational-operators.py
@@ -0,0 +1,22 @@
+a, b = map(int, input().split())
+
+if a >= b:
+    print(1)
+else:
+    print(0)
+
+if a > b:
+    print(1)
+else:
+    print(0)
+
+if a <= b:
+    print(1)
+else:
+    print(0)
+
+if a < b:
+    print(1)
+else:
+    print(0)
+
