04a0c3fba488668ec6e74c2d9d060a093a5fff13
ksy00051087 <ksy00051087@gmail.com>
2026-09-07 09:49:43 +0900

    [Passed] 조건에 부합하는 수 (52ms, 16MB)

---
+++ trail1/조건에 부합하는 수/number-that-meets-the-condition.py
@@ -0,0 +1,8 @@
+
+a = int(input())
+
+for i in range(1, a + 1, 1):
+    if (i % 2 == 1 or i % 4 == 0) and ((i // 8) % 2 != 0) and (i % 7 >= 4 ):
+        print(i, end=' ')
+        continue
+        print(i, end=' ')
