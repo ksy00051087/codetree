cc97378709a8e637baf4f87199c7c2b7dd9bd1f3
ksy00051087 <ksy00051087@gmail.com>
2026-09-09 10:48:47 +0900

    [Passed] 별표 출력하기 4 (48ms, 16MB)

---
+++ trail1/별표 출력하기 4/print-star-4.py
@@ -0,0 +1,10 @@
+N = int(input())
+
+for i in range(N, 1, -1):
+    for j in range(i):
+        print("*", end = ' ')
+    print()
+for i in range(N):
+    for j in range (i + 1):
+        print('*', end = ' ')
+    print()
