322dfb94611026039ac09ac150acaaa318db63ad
ksy00051087 <ksy00051087@gmail.com>
2026-09-09 09:48:07 +0900

    [Passed] 2가 3번째로 등장하는 위치 (54ms, 16MB)

---
+++ trail1/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
@@ -0,0 +1,9 @@
+n=int(input())
+arr = list(map(int, input().split()))
+cnt, result =0, 0
+for idx, el in enumerate(arr):
+    if el == 2:
+        cnt+=1
+        if cnt == 3:
+            result = idx
+print(result+1)   
