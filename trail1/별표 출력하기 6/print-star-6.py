ea58744f73f9b394c22eaaa6f6db2c4342359b09
ksy00051087 <ksy00051087@gmail.com>
2026-09-10 08:53:36 +0900

    [Passed] 별표 출력하기 6 (48ms, 16MB)

--- trail1/별표 출력하기 6/print-star-6.py
+++ trail1/별표 출력하기 6/print-star-6.py
@@ -1,18 +1,3 @@
-# a = int(input())
-
-# for i in range(a):
-#     for j in range(2 * i):
-#         print(" ", end = "")
-#     for j in range((2 * a) - (2 * i) - 1):
-#         print('*', end = ' ')
-#     print()
-
-# for i in range(a - 1):
-#     for j in range(a - (i*2)):
-#         print(" ", end = '')
-#     for j in range(3 + (2 * i)):
-#         print('*', end = ' ')
-#     print()
 
 N = int(input())
 
