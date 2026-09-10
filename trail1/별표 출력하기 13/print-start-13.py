fcf9ec5211caddad7e023e32bb647954bd09efc3
ksy00051087 <ksy00051087@gmail.com>
2026-09-10 10:54:55 +0900

    [Passed] 별표 출력하기 13 (48ms, 16MB)

---
+++ trail1/별표 출력하기 13/print-start-13.py
@@ -0,0 +1,16 @@
+
+
+
+N = int(input())
+
+for i in range(2 * N):
+
+    if i % 2 != 0:
+        for j in range(1 + (i // 2)):
+            print("*", end=" ")
+        print()
+
+    else:
+        for j in range(N - (i // 2)):
+            print("*", end=" ")
+        print()
