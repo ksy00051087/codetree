e4fc276d6ad8e7069df9835b1ca71b2a955dbb6d
ksy00051087 <ksy00051087@gmail.com>
2026-09-14 17:50:51 +0900

    [Passed] 짝수의 합 n번 구하기 (48ms, 16MB)

---
+++ trail1/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+for _ in range(N):
+    arr = input().split()
+    a, b = int(arr[0]), int(arr[1])
+    sum_val = 0
+    for i in range(a, b+ 1):
+        if i % 2 == 0:
+            sum_val += i
+    print(sum_val, end = ' ')
+    print()
