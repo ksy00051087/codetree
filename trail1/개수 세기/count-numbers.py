db322cfac40059a8713433974980fa89c8edea18
ksy00051087 <ksy00051087@gmail.com>
2026-09-17 15:44:39 +0900

    [Passed] 개수 세기 (49ms, 16MB)

--- trail1/개수 세기/count-numbers.py
+++ trail1/개수 세기/count-numbers.py
@@ -1,7 +1,7 @@
-n,m = map(int, input().split())
+N, M = map(int, input().split())
 arr = list(map(int, input().split()))
 cnt = 0
-for i in range(n):
-    if arr[i] == m:
-        cnt += 1 
+for i in arr:
+    if i == M:
+        cnt += 1
 print(cnt)
