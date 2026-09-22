4c686d017d5afa753855d618ca3bd832bba594b1
ksy00051087 <ksy00051087@gmail.com>
2026-09-22 13:39:10 +0900

    [Passed] 지그재그로 정수 채우기 (48ms, 16MB)

--- trail1/지그재그로 정수 채우기/zigzag-numbering.py
+++ trail1/지그재그로 정수 채우기/zigzag-numbering.py
@@ -3,10 +3,8 @@ N, M = map(int, input().split())
 for i in range(N):
     for j in range(M):
         if j % 2 == 0:
-            # 짝수 열 (0, 2...): 위에서 아래로 정상 증가
             val = (j * N) + i
         else:
-            # 홀수 열 (1, 3...): 밑에서 위로 역순 감소
             val = (j * N - 1) + (N - i)
             
         print(val, end=' ')
