f3f2eeb63da30f12552346485a46ea72a2b52dea
ksy00051087 <ksy00051087@gmail.com>
2026-09-22 17:34:48 +0900

    [Passed] 배열의 합 (47ms, 16MB)

--- trail1/배열의 합/sum-of-array.py
+++ trail1/배열의 합/sum-of-array.py
@@ -1,4 +1,3 @@
-arr = [list(map(int,input().split())) for _ in range(4)]
-for i in arr:
-    sum_val = sum(i)
-    print(sum_val)
+arr = [list(map(int, input().split())) for _ in range(4)]
+for i in range(4):
+    print(sum(arr[i]))
