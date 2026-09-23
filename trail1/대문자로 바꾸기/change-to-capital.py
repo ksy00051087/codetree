43d52614f3e2e47d91aa13149684d6fd41787447
ksy00051087 <ksy00051087@gmail.com>
2026-09-23 11:47:41 +0900

    [Passed] 대문자로 바꾸기 (49ms, 16MB)

--- trail1/대문자로 바꾸기/change-to-capital.py
+++ trail1/대문자로 바꾸기/change-to-capital.py
@@ -1,5 +1,16 @@
-arr = [list(map(str, input().split())) for _ in range(5)]
-for i in range(5):
-    for j in range(3): 
-        print(arr[i][j].upper(), end = " ")
+N = 5
+
+arr = [list(map(ord, input().split()))
+for _ in range(N)]
+
+for i in range(N):
+    for j in range(3):
+        a = (arr[i][j] - 32)
+        print(chr(a), end = ' ')
     print()
+
+
+
+# # print(ord(x))        
+# print(chr(x))    
+# # chr(ord(x) + 1) 
